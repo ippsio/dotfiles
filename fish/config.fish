1eb3c523208ba7749cfd3e169d341155ff260b45
ippsio <ippei.shioiri@gmail.com>
2019-05-24 11:26:48 +0900

    剪定

--- fish/config.fish
+++ fish/config.fish
@@ -3,42 +3,6 @@ rbenv init - | source
 alias vi  'nvim'
 alias vim 'nvim'
 
-alias g 'git'
-
-alias gs  'git status'
-alias gst 'git status'
-
-alias gtatus  'git status'
-alias gstatus 'git status'
-
-alias giff  'git diff'
-alias gdiff 'git diff'
-
-alias gerge  'git merge'
-alias gmerge 'git merge'
-
-alias gog  'git log'
-alias glog 'git log'
-alias gogol  'git log --oneline'
-alias glogol 'git log --oneline'
-
-alias gheckout  'git checkout'
-alias gcheckout 'git checkout'
-alias go  'git checkout'
-alias gco 'git checkout'
-
-alias geset  'git reset'
-alias greset 'git reset'
-
-alias getch  'git fetch'
-alias gfetch 'git fetch'
-
-alias gdd  'git add'
-alias gadd 'git add'
-
-alias gommit  'git commit'
-alias gcommit 'git commit'
-
-alias gush  'git push'
-alias gpush 'git push'
+alias g   'git'
+alias gfo 'git fetch origin'
 
