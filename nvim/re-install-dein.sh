6145628a952bbcfdccc91c50ff02a85d76722ea1
ippsio <ippei.shioiri@gmail.com>
2019-05-28 11:15:47 +0900

    剪定

--- nvim/re-install-dein.sh
+++ nvim/re-install-dein.sh
@@ -3,5 +3,4 @@
 rm -rf ~/.cache/dein
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh -o /tmp/dein_installer.sh
 sh /tmp/dein_installer.sh ~/.cache/dein
-rm -rf /tmp/dein_installer.sh
 
