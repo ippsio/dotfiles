237de9ddf3dd96d0167c9da6a0c8207cc23e5316
ippsio <ippei.shioiri@gmail.com>
2020-04-27 11:11:49 +0900

    re-install_dein.sh

--- .config/nvim/re-install-dein.sh
+++ .config/nvim/re-install-dein.sh
@@ -2,8 +2,19 @@
 
 CD=~/.cache/dein
 CDDT=${CD}_$(date +'%Y%m%d_%H%M%S')
+
+echo "rm -r mkdir -p ${CDDT}"
 mkdir -p ${CDDT}
-rm -r ${CD} && ln -sF ${CDDT} ${CD}
+
+echo "rm -r ${CD}"
+rm -rf ${CD}
+
+echo "ln -sF ${CDDT} ${CD}"
+ln -sF ${CDDT} ${CD}
+
+echo "curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh -o ${CD}/installer.sh"
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh -o ${CD}/installer.sh
+
+echo "sh ${CD}/installer.sh ${CD}"
 sh ${CD}/installer.sh ${CD}
 
