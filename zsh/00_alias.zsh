ada4f9ed668a09c8a20f94229b9abf4a221cb203
ippsio <ippei.shioiri@gmail.com>
2020-05-05 23:55:34 +0900

    整理

--- zsh/00_alias.zsh
+++ zsh/00_alias.zsh
@@ -3,14 +3,16 @@ alias ls='ls -G'
 alias vim='nvim'
 alias v='nvim'
 function _todays_memo() {
+  MEMO_NAME=$1
   YMDA=$(date '+%Y/%m/%d(%a)')
-  DIR=~/memo/$(date '+%Y_%m')
+  DIR=~/${MEMO_NAME}/$(date '+%Y_%m')
   FILE=${DIR}/$(date '+%Y_%m%d').md
   mkdir -p ${DIR}
-  [ ! -f ${FILE} ] && echo "# ${YMDA}\n---\n" >> ${FILE}
-  nvim ${FILE} +3
+  [ ! -f ${FILE} ] && echo "# ${YMDA}\n${MEMO_NAME}\n---\n" >> ${FILE}
+  nvim ${FILE} +4
 }
-alias m=_todays_memo
+alias m="_todays_memo memo"
+alias n"=_todays_memo note"
 
 # global arial
 alias -g D='-w --reverse $(git merge-base develop HEAD)...HEAD'
