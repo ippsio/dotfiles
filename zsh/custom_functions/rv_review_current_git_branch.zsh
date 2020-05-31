2e1470621f39ca2a0d8e3797a2532f225f1c16ed
ippsio <ippei.shioiri@gmail.com>
2020-06-01 08:53:05 +0900

    change for readability.

--- zsh/custom_functions/rv_review_current_git_branch.zsh
+++ zsh/custom_functions/rv_review_current_git_branch.zsh
@@ -39,21 +39,21 @@ function _fzf_with_preview_git_diff() {
       # そのファイルのdiff
       ;echo {} | sed -e 's/.*, //g'|xargs git diff --color=always ${merge_base}...HEAD| diff-highlight| less \
     " \
-    --preview-window=right:60%:wrap \
+    --preview-window=bottom:60%:wrap \
     | sed -e 's/.*, //g' \
   )
 }
 
 # rv[enter]で現在のgitブランチのレビューを開始
 function review_current_git_branch() {
-  mbb=${1:-origin/develop}
-  mb=$(git merge-base ${mbb} HEAD)
+  merge_base_branch=${1:-origin/develop}
+  merge_base_commit=$(git merge-base ${merge_base_branch} HEAD)
 
   while true; do
     clear
     # git merge-base
-    printf "\e[33mMERGE-BASE=${mbb}\e[m\n"
-    printf "\e[33m\"git merge-base ${mbb} HEAD\" = ${mb}\e[m\n\n"
+    printf "\e[33mMERGE-BASE=${merge_base_branch}\e[m\n"
+    printf "\e[33m\"git merge-base ${merge_base_branch} HEAD\" = ${merge_base_commit}\e[m\n\n"
 
     local commit_hash="%Cred%h%Creset"
     local author="%C(bold blue)%an%Creset"
@@ -63,32 +63,32 @@ function review_current_git_branch() {
 
     # git log
     printf "\e[33;7m[git log]\e[m\n"
-    git --no-pager log $(git merge-base ${mb} HEAD)...HEAD --reverse --oneline -5 --color=always \
+    git --no-pager log $(git merge-base ${merge_base_commit} HEAD)...HEAD --reverse --oneline -5 --color=always \
       --date=format-local:'%Y/%m/%d %H:%M:%S' \
       --pretty=format:"${commit_hash}${commit_date} ${author}${ref_names} ${subject}" \
       --abbrev-commit
-    printf "\n\e[33m- total commit count $(git --no-pager log --oneline ${mb}...HEAD| wc -l| sed -e 's/ //g'). \n\n\e[m"
+    printf "\n\e[33m- total commit count $(git --no-pager log --oneline ${merge_base_commit}...HEAD| wc -l| sed -e 's/ //g'). \n\n\e[m"
 
     # File changed
     printf "\e[33;7m[file changed]\e[m\n"
-    git diff --stat $(git merge-base ${mb} HEAD)...HEAD
-    printf "\e[33m- $(git diff --name-only ${mb}...HEAD| wc -l| sed -e 's/ //g') files\n\n\e[m"
+    git diff --stat $(git merge-base ${merge_base_commit} HEAD)...HEAD
+    printf "\e[33m- $(git diff --name-only ${merge_base_commit}...HEAD| wc -l| sed -e 's/ //g') files\n\n\e[m"
 
     # Command
     printf "\e[33;7m[Command]\e[m\n"
-    echo "1 | git diff ) git diff \$(git merge-base ${mbb} HEAD)...HEAD"
+    echo "1 | d | git diff ) git diff \$(git merge-base ${merge_base_branch} HEAD)...HEAD"
     echo "3 | v | vim | [enter] ) vim"
     echo "4 | t ) tig file"
-    echo "5 | tig ) tig -w --reverse \$(git merge-base ${mbb} HEAD)...HEAD"
+    echo "5 | tig ) tig -w --reverse \$(git merge-base ${merge_base_branch} HEAD)...HEAD"
     echo "9 | q | ctrl-c ) bye"
     echo -n " > "
     read REPLY
     case "${REPLY}" in
-      1 ) git diff $(git merge-base ${mbb} HEAD)...HEAD ;;
+      1 ) git diff $(git merge-base ${merge_base_branch} HEAD)...HEAD ;;
       3 | v | vim ) f=$(_fzf_with_preview_git_diff) && [ ! -z $f ] && vim $f ;;
-      4 | t )       f=$(_fzf_with_preview_git_diff) && [ ! -z $f ] && tig -w --reverse $(git merge-base ${mbb} HEAD)...HEAD $f ;;
-      5 | tig | t ) tig -w --reverse $(git merge-base ${mbb} HEAD)...HEAD ;;
-      5 | tigall | t ) tig -w --reverse $(git merge-base ${mbb} HEAD)...HEAD ;;
+      4 | t )       f=$(_fzf_with_preview_git_diff) && [ ! -z $f ] && tig -w --reverse $(git merge-base ${merge_base_branch} HEAD)...HEAD $f ;;
+      5 | tig | t ) tig -w --reverse $(git merge-base ${merge_base_branch} HEAD)...HEAD ;;
+      5 | tigall | t ) tig -w --reverse $(git merge-base ${merge_base_branch} HEAD)...HEAD ;;
       9 | q | ctrl-c ) break ;;
 
     esac
