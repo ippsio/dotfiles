4b50b0c8941f7e2fa824d7fbf38f9d835aa91403
ippsio <ippei.shioiri@gmail.com>
2020-06-01 09:10:41 +0900

    change for readability.

--- zsh/custom_functions/rv_review_current_git_branch.zsh
+++ zsh/custom_functions/rv_review_current_git_branch.zsh
@@ -1,5 +1,6 @@
 function _fzf_with_preview_git_diff() {
-  merge_base="$(git merge-base ${1:-origin/develop} HEAD)"
+  merge_base_branch=${1:-origin/develop}
+  merge_base_commit=$(git merge-base ${merge_base_branch} HEAD)
   local commit_hash="%Cred%h%Creset"
   local author="%C(bold blue)%an%Creset"
   local subject="%s"
@@ -7,7 +8,7 @@ function _fzf_with_preview_git_diff() {
   local ref_names="%C(yellow)%d%Creset"
 
   echo $(\
-    git diff --numstat ${merge_base}...HEAD \
+    git diff --numstat ${merge_base_commit}...HEAD \
     | awk '\
       {printf "%+5s" , "+" $1} \
       {printf "%+5s" , "-" $2} \
@@ -17,13 +18,13 @@ function _fzf_with_preview_git_diff() {
     --preview " \
       # 全コミット数
       echo {} | sed -e 's/.*, //g' \
-        | xargs git log ${merge_base}...HEAD --oneline \
+        | xargs git log ${merge_base_commit}...HEAD --oneline \
         | wc -l \
         | tr -d ' ' \
         | sed -e 's/$/ total commits on this branch./' \
       # 直近3コミット
       ;echo {} | sed -e 's/.*, //g' | \
-        xargs git log ${merge_base}...HEAD --oneline -3 \
+        xargs git log ${merge_base_commit}...HEAD --oneline -3 \
         --color=always --decorate=full \
         --date=format-local:'%Y/%m/%d %H:%M:%S' \
         --pretty=format:'${commit_hash}${commit_date} ${author}${ref_names} ${subject}' \
@@ -33,11 +34,11 @@ function _fzf_with_preview_git_diff() {
       ;echo \
       # そのファイルのdiffstat
       ;echo {} | sed -e 's/.*, //g' \
-        |xargs git diff --color=always --stat ${merge_base}...HEAD \
+        |xargs git diff --color=always --stat ${merge_base_commit}...HEAD \
       # 改行
       ;echo \
       # そのファイルのdiff
-      ;echo {} | sed -e 's/.*, //g'|xargs git diff --color=always ${merge_base}...HEAD| diff-highlight| less \
+      ;echo {} | sed -e 's/.*, //g'|xargs git diff --color=always ${merge_base_commit}...HEAD| diff-highlight| less \
     " \
     --preview-window=bottom:60%:wrap \
     | sed -e 's/.*, //g' \
@@ -76,20 +77,17 @@ function review_current_git_branch() {
 
     # Command
     printf "\e[33;7m[Command]\e[m\n"
-    echo "1 | d | git diff ) git diff \$(git merge-base ${merge_base_branch} HEAD)...HEAD"
-    echo "3 | v | vim | [enter] ) vim"
-    echo "4 | t ) tig file"
-    echo "5 | tig ) tig -w --reverse \$(git merge-base ${merge_base_branch} HEAD)...HEAD"
-    echo "9 | q | ctrl-c ) bye"
+    echo "1 | d | diff ) git diff \$(git merge-base ${merge_base_branch} HEAD)...HEAD"
+    echo "2 | v ) open file with vim"
+    echo "3 | t ) open file with tig"
+    echo "4 | tig ) tig -w --reverse \$(git merge-base ${merge_base_branch} HEAD)...HEAD"
     echo -n " > "
     read REPLY
     case "${REPLY}" in
-      1 ) git diff $(git merge-base ${merge_base_branch} HEAD)...HEAD ;;
-      3 | v | vim ) f=$(_fzf_with_preview_git_diff) && [ ! -z $f ] && vim $f ;;
-      4 | t )       f=$(_fzf_with_preview_git_diff) && [ ! -z $f ] && tig -w --reverse $(git merge-base ${merge_base_branch} HEAD)...HEAD $f ;;
-      5 | tig | t ) tig -w --reverse $(git merge-base ${merge_base_branch} HEAD)...HEAD ;;
-      5 | tigall | t ) tig -w --reverse $(git merge-base ${merge_base_branch} HEAD)...HEAD ;;
-      9 | q | ctrl-c ) break ;;
+      1 | d | diff) git diff $(git merge-base ${merge_base_branch} HEAD)...HEAD | vim -R;;
+      2 | v       ) file=$(_fzf_with_preview_git_diff) && [ ! -z $file ] && vim $file ;;
+      3 | t       ) file=$(_fzf_with_preview_git_diff) && [ ! -z $file ] && tig -w --reverse $(git merge-base ${merge_base_branch} HEAD)...HEAD $file ;;
+      4 | tig     ) tig -w --reverse $(git merge-base ${merge_base_branch} HEAD)...HEAD ;;
 
     esac
   done
