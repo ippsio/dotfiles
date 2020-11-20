e51a324f0bb578199e3eb83ce6539bf00f165f05
ippsio <ippei.shioiri@gmail.com>
2020-11-20 14:31:36 +0900

    剪定

--- zsh/custom_functions/fzf_edit_memo.zsh
+++ zsh/custom_functions/fzf_edit_memo.zsh
@@ -1,8 +1,10 @@
 function memo() {
-  NOTE_TYPE=$(echo "memo\n日報"|fzf --prompt="SELECT NOTE> ")
+  #NOTE_TYPE=$(echo "memo\ntodo\n日報"|fzf --prompt="SELECT NOTE> ")
+  # 面倒くせえからmemo一択にした。
+  NOTE_TYPE=memo
   echo "NOTE_TYPE=${NOTE_TYPE}"
 
-  VALID_ARGS=("memo" "日報")
+  VALID_ARGS=("memo" "todo" "日報")
   if [[ -n ${VALID_ARGS[(re)$NOTE_TYPE]} ]]; then
     YMDA=$(date '+%Y/%m/%d(%a)')
     NOTE_DIR=${ENV_ROOT_NOTES_DIR:-~/notes}/${NOTE_TYPE}/$(date '+%Y_%m')
@@ -10,7 +12,7 @@ function memo() {
     echo "FILE=$TODAYS_NOTE_FILE"
     mkdir -p ${NOTE_DIR}
     [ ! -f ${TODAYS_NOTE_FILE} ] && echo "# ${YMDA}\n${NOTE_TYPE}\n---\n" >> ${TODAYS_NOTE_FILE}
-    notefile=$(find ${ENV_ROOT_NOTES_DIR:-~/notes}/ -type f| fgrep "${NOTE_TYPE}" |sort -r | fzf --prompt="${NOTE_TYPE} >" --preview='bat {}')
+    notefile=$(find ${ENV_ROOT_NOTES_DIR:-~/notes}/ -type f| fgrep "${NOTE_TYPE}" |sort -r | fzf --prompt="${NOTE_TYPE} >" --preview='bat {} --color=always')
     if [[ "${notefile}" != "" ]]; then
       nvim ${notefile} +4
     fi
