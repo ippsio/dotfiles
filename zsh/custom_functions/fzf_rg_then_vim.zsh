ab1b06afa9a35cd57447e2e9b05f7e866d18c51c
ippsio <ippei.shioiri@gmail.com>
2020-12-10 12:03:02 +0900

    剪定

--- zsh/custom_functions/fzf_rg_then_vim.zsh
+++ zsh/custom_functions/fzf_rg_then_vim.zsh
@@ -1,5 +1,6 @@
 # rgの検索結果を選択I/Fを表示する。
 # さらに、選択したファイルはvimで開く
+# fzfのプレビューは、基本的に検索ヒットした行の5行前から表示する(decide_from)。
 SH_NAME=$(basename $0)
 function fzf_rg_then_vim() {
   [ -z "$1" ] && echo 'Usage: rgg PATTERN' && return 0
@@ -8,7 +9,7 @@ function fzf_rg_then_vim() {
     | fzf -m \
       --prompt="${SH_NAME}> " \
       --bind "enter:execute(nvim {1} +{2} < /dev/tty > /dev/tty)" \
-      --preview 'decide_from() { echo $(( $1 - 5 > 0 ? $1 - 5 : 1)) }; bat --color=always --style=numbers,header,grid --line-range $(decide_from {2}): --highlight-line {2} {1}'
+      --preview 'decide_range_from() { echo $(( $1 - 5 > 0 ? $1 - 5 : 1)) }; bat --color=always --style=numbers,header,grid --line-range $(decide_range_from {2}): --highlight-line {2} {1}'
   )
 }
 
