efa571b4640119b688462a3ef28e1446b5a53d6e
ippsio <ippei.shioiri@gmail.com>
2021-05-26 13:04:37 +0900

    update tmux.conf and prompt

--- zsh/10_prompt.zsh
+++ zsh/10_prompt.zsh
@@ -42,35 +42,35 @@ function _precmd_vcs_info_msg() {
 add-zsh-hook precmd _precmd_vcs_info_msg
 
 # MARK DEFINITIONS
-local _text_unstaged="UNSTAGED:"
+local _text_unstaged="wtree"
 local _mark_deleted="x"
 local _mark_untracked="?"
 local _mark_unstaged="-"
-local _text_staged="STAGED:"
+local _text_staged="stage"
 local _mark_staged="+"
-local _text_repo="REPO:"
+local _text_repo="repo"
 local _mark_ahead="^"
 local _mark_behind="v"
 local _mark_stash="stash"
 
 # exit status and git basic infomations.
 local EXIT_CD="%K{red}%(?.. [EXIT_CD=%?])%k"
-local GIT_REPO_NAME="%K{24}%(1v|[%1v|)%k"
-local GIT_BRANCH="%K{24}%(2v|(%2v)]|)%k"
+local GIT_REPO_NAME="%K{24}%F{255}%(1v|[%1v|)%f%k"
+local GIT_BRANCH="%K{24}%F{255}%(2v|(%2v)]|)%f%k"
 
 # git working_tree
-local _deleted="%(3v|[${_text_unstaged}${_mark_deleted}%3v|)"
+local _deleted="%(3v|${_text_unstaged}[${_mark_deleted}%3v|)"
 local _untracked="%(4v| ${_mark_untracked}%4v|)"
-local _unstaged="%(5v| ${_mark_unstaged}%5v] |)"
-local GIT_WORKING_TREE="%K{94}${_deleted}${_untracked}${_unstaged}%k"
+local _unstaged="%(5v| ${_mark_unstaged}%5v]|)"
+local GIT_WORKING_TREE="%K{94}%F{255}${_deleted}${_untracked}${_unstaged}%f%k"
 
 # git stage
-local _staged="%(6v| [${_text_staged}${_mark_staged}%6v] |)%k"
+local _staged="%(6v| ${_text_staged}[${_mark_staged}%6v]|)%k"
 local GIT_STAGE="%K{90}%F{255}${_staged}%f%k"
 
 # git local repositry
-local _ahead="%(7v|[${_text_repo}%7v${_mark_ahead}|)"
-local _behind="%(8v| ${_mark_behind}%8v] |)"
+local _ahead="%(7v| ${_text_repo}[${_mark_ahead}%7v|)"
+local _behind="%(8v| ${_mark_behind}%8v]|)"
 local GIT_LOCAL_REPO="%K{88}%F{255}${_ahead}${_behind}%f%k"
 
 local _stash="%(9v| ${_mark_stash}(%9v) |)"
@@ -79,8 +79,8 @@ local _more_such_as_rebase="%(10v| %10v |)%(11v| %11v |)%(12v| %12v |)%(13v| %13
 local GIT_CAUTION="%K{18}${_stash}${_more_such_as_rebase}%k"
 
 # others
-local CURRENT_DIRECTORY="%K{237}[%~] %k"
-local CURRENT_DATETIME="%K{237}%D{%m/%d %T} %k"
+local CURRENT_DIRECTORY="%K{237}%F{255}[%~] %f%k"
+local CURRENT_DATETIME="%K{237}%F{255}%D{%m/%d %T} %f%k"
 
 local NUMBER_OF_JOBS="%(1j|%F{226}BackgroundJobs(%j)%f|)"
 
