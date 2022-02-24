be09e0834843a995ae40e970f316dac4c535a134
ippsio <ippei.shioiri@gmail.com>
2022-02-24 13:16:15 +0900

    update

--- zshrc/50_existing_command_hacking.zsh
+++ zshrc/50_existing_command_hacking.zsh
@@ -2,5 +2,5 @@
 # not sure why but the -M flag breaks it but it's not required
 # Seems like nvim already sets -M when ft=man
 man() {
-  /usr/bin/man $* -P 'col -b | nvim -R -c "set ft=man ts=8 nomod nolist nospell nu" -c "nun <buffer> q" -c "nn q :qa!<CR>" -'
+  /usr/bin/man $* -P 'col -b | nvim -R -c "set ft=man ts=8 nomod nolist nospell nu" -c "nun <buffer> q" -c "nn q :qa!<CR>" -';
 }
