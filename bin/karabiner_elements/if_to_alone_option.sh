9407d754065f0649644cc1a5bdc4ed91d06f5c2e
ippsio <1188686+ippsio@users.noreply.github.com>
2024-02-03 14:05:22 +0900

    update

--- bin/karabiner_elements/if_to_alone_option.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-osascript ~/dotfiles/bin/karabiner_elements/if_to_alone_option.scpt Alacritty
---
+++ bin/karabiner_elements/to_if_alone_option.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+osascript ~/dotfiles/bin/karabiner_elements/to_if_alone_option.scpt Alacritty
