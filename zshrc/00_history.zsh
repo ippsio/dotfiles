c85a59ab619bb8f163760daf64be14df5e05ea26
ippsio <i@localhost>
2025-10-28 11:28:28 +0900

    update

--- zshrc/00_history.zsh
+++ zshrc/00_history.zsh
@@ -5,3 +5,11 @@ HISTFILE=~/.zsh_history
 HISTSIZE=10000 # メモリに保存される履歴の件数
 SAVEHIST=10000 # 履歴ファイルに保存される履歴の件数
 
+zshaddhistory() {
+  if [[ $1 == ';'* ]]; then
+    # 先頭が ';' で始まる場合は履歴に追加しない
+    return 1
+  else
+    return 0
+  fi
+}
