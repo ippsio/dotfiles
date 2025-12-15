d68d8c692f66f5695419a6afdb3ab9af4ba6305d
ippsio <i@localhost>
2025-12-16 08:08:48 +0900

    update

--- zshrc/10_prepare.zsh
+++ zshrc/10_prepare.zsh
@@ -6,10 +6,16 @@ nodir_then_gitclone() {
   [ -d $1 ] || (set -x; git clone https://github.com/$2 ${3:-$1})
 }
 may_ln_file() {
-  [ -L $1 ] || (set -x; ln -s $2 $1)
+  [ -L "${1/dotfiles\//}" ] || (set -x; ln -s "$1" "${1/dotfiles\//}")
 }
 may_ln_dir() {
-  [ -d $1 ] || (set -x; ln -s $2 $1)
+  [ -d "${1/dotfiles\//}" ] || (set -x; ln -s "$1" "${1/dotfiles\//}")
+}
+ensure_dir() {
+  [ -d $1 ] || mkdir -p $1
+}
+ensure_file() {
+  [ -f $1 ] || touch $1
 }
 
 may_brew_install nvim neovim
@@ -34,24 +40,23 @@ may_brew_install tmux
 
 nodir_then_gitclone "${TMUX_PLUGINS}/tpm" "tmux-plugins/tpm"
 
-[ -d $HOME/.tmux/log/ ] || mkdir -p $HOME/.tmux/log/
-[ -d $HOME/.local/share/tig ] || mkdir -p $HOME/.local/share/tig
-[ -d $HOME/.config/karabiner/assets ] || mkdir -p $HOME/.config/karabiner/assets
+ensure_dir $HOME/.tmux/log
+ensure_dir $HOME/.local/share/tig
+ensure_dir $HOME/.config/karabiner/assets
+ensure_file $HOME/.local/share/tig/history
 
-[ -f $HOME/.local/share/tig/history ] || touch $HOME/.local/share/tig/history
+may_ln_dir $HOME/dotfiles/.config/alacritty
+may_ln_dir $HOME/dotfiles/.config/bat
+may_ln_dir $HOME/dotfiles/.config/direnv
+may_ln_dir $HOME/dotfiles/.config/karabiner/assets/complex_modifications
+may_ln_dir $HOME/dotfiles/.config/kitty
+may_ln_dir $HOME/dotfiles/.config/nvim
+may_ln_dir $HOME/dotfiles/.config/rio
+may_ln_dir $HOME/dotfiles/.config/tig
 
-may_ln_dir $HOME/.config/nvim $HOME/dotfiles/.config/nvim
-may_ln_dir $HOME/.config/bat $HOME/dotfiles/.config/bat
-may_ln_dir $HOME/.config/alacritty $HOME/dotfiles/.config/alacritty
-may_ln_dir $HOME/.config/tig $HOME/dotfiles/.config/tig
-may_ln_dir $HOME/.config/direnv $HOME/dotfiles/.config/direnv
-may_ln_dir $HOME/.config/kitty $HOME/dotfiles/.config/kitty
-may_ln_dir $HOME/.config/karabiner/assets/complex_modifications $HOME/dotfiles/.config/karabiner/assets/complex_modifications
-
-may_ln_file $HOME/.gitattributes_global $HOME/dotfiles/.gitattributes_global
-may_ln_file $HOME/.gitconfig $HOME/dotfiles/.gitconfig
-may_ln_file $HOME/.gitignore_global $HOME/dotfiles/.gitignore_global
-may_ln_file $HOME/.pryrc $HOME/dotfiles/.pryrc
-may_ln_file $HOME/.tigrc $HOME/dotfiles/.tigrc
-may_ln_file $HOME/.tmux.conf $HOME/dotfiles/.tmux.conf
-may_ln_file $HOME/.zshrc $HOME/dotfiles/.zshrc
+may_ln_file $HOME/dotfiles/.gitconfig
+may_ln_file $HOME/dotfiles/.gitignore_global
+may_ln_file $HOME/dotfiles/.pryrc
+may_ln_file $HOME/dotfiles/.tigrc
+may_ln_file $HOME/dotfiles/.tmux.conf
+may_ln_file $HOME/dotfiles/.zshrc
