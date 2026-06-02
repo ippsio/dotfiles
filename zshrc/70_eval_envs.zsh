f0da5b873ec32c8e6d2c94b9653dc17541177c2b
ippsio <1188686+ippsio@users.noreply.github.com>
2026-06-03 00:17:22 +0900

    update

--- zshrc/70_eval_envs.zsh
+++ zshrc/70_eval_envs.zsh
@@ -2,12 +2,10 @@
 NAME="${0/#$HOME/\$HOME}"
 nodenv_init_lazy() {
   type nodenv >/dev/null || return 1
-  echo "*** $NAME nodenv lazy init ***"
-  printf 'eval "$(nodenv init -)" '
   st=$(epocms)
   eval "$(nodenv init -)"
   et=$(epocms)
-  printf "=> %d ms\n" $(( et - st ))
+  printf '*** %s nodenv lazy init *** eval "$(nodenv init -)" => %d ms\n' "$NAME" $(( et - st ))
   unset -f nodenv_init_lazy
   unset -f node
   unset -f npm
@@ -16,12 +14,10 @@ nodenv_init_lazy() {
 }
 pyenv_init_lazy() {
   type pyenv >/dev/null || return 1
-  echo "*** $NAME pyenv lazy init ***"
-  printf 'eval "$(pyenv init --path)" '
   st=$(epocms)
   eval "$(pyenv init --path)"
   et=$(epocms)
-  printf "=> %d ms\n" $(( et - st ))
+  printf '*** %s pyenv lazy init *** eval "$(pyenv init --path)" => %d ms\n' "$NAME" $(( et - st ))
   unset -f pyenv_init_lazy
   unset -f pip
   unset -f pip3
@@ -31,12 +27,10 @@ pyenv_init_lazy() {
 }
 rbenv_init_lazy() {
   type rbenv >/dev/null || return 1
-  echo "*** $NAME rbenv lazy init ***"
-  printf 'eval "$(rbenv init -)" '
   st=$(epocms)
   eval "$(rbenv init -)"
   et=$(epocms)
-  printf "=> %d ms\n" $(( et - st ))
+  printf '*** %s rbenv lazy init *** eval "$(rbenv init -)" => %d ms\n' "$NAME" $(( et - st ))
   unset -f rbenv_init_lazy
   unset -f bundle
   unset -f gem
